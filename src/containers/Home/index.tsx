0afd10ca94e47ae0000c3426043d82af78bf8cdb
Joao Tavares <joao.tavares@cubos.io>
2018-06-19 22:07:54 -0300

    CHANGE: initial commit methods and tags removed, landpage created

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,43 +1,50 @@
 import * as React from "react";
 import { inject, observer } from "mobx-react";
-
-import { CounterStore } from "../../stores";
+import { Button } from "antd";
+import { RouterStore } from "mobx-react-router";
 
 const s = require("./style.scss");
+const photo = require("../../assets/img/personalPhoto.jpg");
 
 interface IProps {
-	counterStore: CounterStore;
+	routerStore: RouterStore;
 }
 
-@inject("counterStore")
+@inject("routerStore")
 @observer
 export class Home extends React.Component<IProps, {}> {
-
-	private incrementCount = () => {
-		this.props.counterStore.incrementCount();
-	}
-
-	private decrementCount = () => {
-		this.props.counterStore.decrementCount();
-	}
-
-	private resetCount = () => {
-		this.props.counterStore.resetCount();
-	}
-
+	private goTo = (path: string) => {
+		this.props.routerStore.push(path);
+	};
 	public render() {
-		const formatedCount = this.props.counterStore.formatedCount;
-
 		return (
 			<div className={s.container}>
-				<div className={s.home}>
-					<div className={s.counter}>
-						<span className={s.count}>{formatedCount}</span>
-						<div className={s.rowButton}>
-							<button className={s.button} onClick={this.decrementCount}>Decrement</button>
-							<button className={s.button} onClick={this.resetCount}>Reset</button>
-							<button className={s.button} onClick={this.incrementCount}>Increment</button>
-						</div>
+				<div className={s.contentBox}>
+					<div className={s.photoDiv}>
+						<img src={photo} className={s.photo} alt="Avatar" />
+					</div>
+					<label className={s.namePhrase}>Olá! Eu sou o João Tavares</label>
+					<label className={s.welcomePhrase}>
+						Seja bem-vindo ao meu site pessoal
+					</label>
+					<label className={s.welcomePhrase}>
+						Fique a vontade para escolher o caminho da sua visita
+					</label>
+					<div className={s.buttonsRow}>
+						<Button
+							type="primary"
+							className={s.buttonProfile}
+							onClick={() => this.goTo("/profile")}
+						>
+							Perfil <br /> Profissional
+						</Button>
+						<Button
+							type="primary"
+							className={s.buttonBlog}
+							onClick={() => this.goTo("/")}
+						>
+							Blog <br /> Pessoal
+						</Button>
 					</div>
 				</div>
 			</div>
