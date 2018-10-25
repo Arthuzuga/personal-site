ba4ff857a186710d6e35d6288e3cf3bc561e77a8
Joao Tavares <joao.tavares@cubos.io>
2018-10-25 17:06:06 -0300

    CREATE: Timeline Section Container in order to convert code to component

---
+++ src/containers/TimelineSection/index.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import { Timeline } from "antd";
+import { Section } from "react-fullpage";
+import "../../antd.css";
+import { timelineSteps } from "../../utils/infos/timelineData";
+import { TimelineItem } from "../../components/TimelineItem";
+const s = require("./style.scss");
+
+export class TimelineSection extends React.Component<{}, {}> {
+	public render() {
+		return (
+			<Section style={{ height: "100vh" }}>
+				<div className={s.sectionDivLight}>
+					<h1>Histórico Profissional</h1>
+					<h2>Conheça um pouco da minha trajetória.</h2>
+					<div className={s.timelineBox}>
+						<Timeline>
+							{timelineSteps.map((item, index) => (
+								<TimelineItem item={item} index={index} />
+							))}
+						</Timeline>
+					</div>
+				</div>
+			</Section>
+		);
+	}
+}
